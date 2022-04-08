7e75b36299e718b8fe7a3d52a59e628e605cfa4d
Nicholas Wagner <nicholas.wagner@havan.com.br>
2022-04-08 15:26:21 -0300

    Navigation

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pager } from "../../components/Pager";
@@ -20,15 +21,17 @@ export default function UserList() {
           <Flex mb='8' justify='space-between' alignItems='center'>
             <Heading size='lg' fontWeight='normal'>Usuários</Heading>
             
-            <Button
-              as='a'
-              size='sm'
-              fontSize='sm'
-              colorScheme='pink'
-              leftIcon={<Icon as={RiAddLine} fontSize='20' />}
-            >
-              Criar novo
-            </Button>
+            <Link href='/users/create' passHref>
+              <Button
+                as='a'
+                size='sm'
+                fontSize='sm'
+                colorScheme='pink'
+                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table
