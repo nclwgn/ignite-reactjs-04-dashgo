7c3e477448deaf8ff4e19da8f3cfe1136ef861fd
Nicholas W <nclwgn@gmail.com>
2022-04-18 22:22:53 -0300

    Configure React Query devtools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,18 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => ({
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+    }));
+
+    return users;
+  }, {
+    staleTime: 1000 * 5, // 5 seconds
   });
 
   const isWideVersion = useBreakpointValue({
@@ -66,42 +77,20 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Nicholas Wagner</Text>
-                        <Text fontSize='sm' color='gray.300'>nclwgn@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>02 de Abril, 2022</Td> }
-                  </Tr>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Nicholas Wagner</Text>
-                        <Text fontSize='sm' color='gray.300'>nclwgn@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>02 de Abril, 2022</Td> }
-                  </Tr>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Nicholas Wagner</Text>
-                        <Text fontSize='sm' color='gray.300'>nclwgn@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>02 de Abril, 2022</Td> }
-                  </Tr>
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                        </Box>
+                      </Td>
+                      { isWideVersion && <Td>{user.createdAt}</Td> }
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
 
